3cdab883d819ff148e37d8a427a0eb0f477c0b3b
ben-m-m <mainaben23@gmail.com>
2023-10-16 09:01:20 +0300

    pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,29 +5,29 @@ The function creates a Pascal's triangle of n rows and returns it as a list
 
 
 def pascal_triangle(n):
-    # Check for invalid input
+    """Check for invalid input"""
     if n <= 0:
         return []
 
-    # Initialize Pascal's triangle with the first row
+    """Initialize Pascal's triangle with the first row"""
     triangle = [[1]]
 
-    # Generate subsequent rows
+    """Generate subsequent rows"""
     for i in range(1, n):
-        # Calculate the next row based on the previous row
+        """Calculate the next row based on the previous row"""
         prev_row = triangle[-1]
-        next_row = [1]  # The first element is always 1
+        next_row = [1]  """The first element is always 1"""
 
         for j in range(1, i):
             next_element = prev_row[j - 1] + prev_row[j]
             next_row.append(next_element)
 
-        next_row.append(1)  # The last element is always 1
+        next_row.append(1)  """The last element is always 1"""
         triangle.append(next_row)
 
     return triangle
 
-# Test the function
+"""Test the function"""
 
 
 if __name__ == "__main__":
