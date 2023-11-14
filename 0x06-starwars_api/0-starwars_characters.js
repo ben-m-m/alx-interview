323818ee8923552143aaee4a63a63722d684210f
ben-m-m <mainaben23@gmail.com>
2023-11-14 13:34:03 +0300

    star wars api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,42 +1,26 @@
 #!/usr/bin/node
+
 const request = require('request');
+const API_URL = 'https://swapi-api.alx-tools.com/';
 
-const movieId = process.argv[2];
-
-if (!movieId) {
-  console.error('Please provide a Movie ID as a command-line argument');
-  process.exit(1);
-}
-
-const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
-
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-  } else if (response.statusCode !== 200) {
-    console.error('Unexpected status code:', response.statusCode);
-  } else {
-    const film = JSON.parse(body);
-
-    if (!film.characters || film.characters.length === 0) {
-      console.log('No characters found for this movie.');
-    } else {
-      fetchCharacters(film.characters);
+if (process.argv.length > 2) {
+  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
+    if (err) {
+      console.log(err);
     }
-  }
-});
+    const charactersURL = JSON.parse(body).characters;
+    const charactersName = charactersURL.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (promiseErr, __, charactersReqBody) => {
+          if (promiseErr) {
+            reject(promiseErr);
+          }
+          resolve(JSON.parse(charactersReqBody).name);
+        });
+      }));
 
-function fetchCharacters (characterUrls) {
-  characterUrls.forEach((characterUrl) => {
-    request(characterUrl, (error, response, body) => {
-      if (error) {
-        console.error('Error:', error);
-      } else if (response.statusCode !== 200) {
-        console.error('Unexpected status code:', response.statusCode);
-      } else {
-        const character = JSON.parse(body);
-        console.log(character.name);
-      }
-    });
+    Promise.all(charactersName)
+      .then(names => console.log(names.join('\n')))
+      .catch(allErr => console.log(allErr));
   });
 }
