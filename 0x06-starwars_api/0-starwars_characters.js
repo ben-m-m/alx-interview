c6f24abfbc4ffde412b1fe507bae90ef8a43186c
ben-m-m <mainaben23@gmail.com>
2023-11-14 14:01:33 +0300

    star wars api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,23 +4,34 @@ const request = require('request');
 const API_URL = 'https://swapi-api.alx-tools.com/';
 
 if (process.argv.length > 2) {
-  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
+  const movieId = process.argv[2];
+  request(`${API_URL}/films/${movieId}/`, (err, response, body) => {
     if (err) {
-      console.log(err);
+      return console.error(err);
     }
-    const charactersURL = JSON.parse(body).characters;
-    const charactersName = charactersURL.map(
-      url => new Promise((resolve, reject) => {
-        request(url, (promiseErr, __, charactersReqBody) => {
-          if (promiseErr) {
-            reject(promiseErr);
-          }
-          resolve(JSON.parse(charactersReqBody).name);
+    if (response.statusCode === 200) {
+      const charactersURLs = JSON.parse(body).characters;
+      const characterPromises = charactersURLs.map(url => {
+        return new Promise((resolve, reject) => {
+          request(url, (error, res, charBody) => {
+            if (error) {
+              reject(error);
+            } else {
+              resolve(JSON.parse(charBody).name);
+            }
+          });
         });
-      }));
+      });
 
-    Promise.all(charactersName)
-      .then(names => console.log(names.join('\n')))
-      .catch(allErr => console.log(allErr));
+      Promise.all(characterPromises)
+        .then(names => {
+          names.forEach(name => console.log(name));
+        })
+        .catch(error => console.error(error));
+    } else {
+      console.error(`Failed to get film data: Status code ${response.statusCode}`);
+    }
   });
+} else {
+  console.log('Usage: ./script.js <movie_id>');
 }
